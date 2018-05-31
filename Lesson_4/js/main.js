ba827c0c256b937636dedb0bd89572a012260e1b
Павел <pelfimov@ya.ru>
2018-05-31 22:08:38 +0700

    Сделал задени №1

--- Lesson_4/js/main.js
+++ Lesson_4/js/main.js
@@ -2,23 +2,35 @@ function getRndNumber(){
   return Math.floor((Math.random() * 6) + 1);;
 };
 
+function printLine(id, string){
+  document.getElementById(id).innerHTML += string + "</br>";
+};
+
+function isNumbersEqual(first, second) {
+  if (first == second) {
+    string = "Выпал дубль. Число " + first;
+    printLine("result", string);
+  }
+  if (first < 3 && second > 4 ) {
+    var summ = second - first;
+    string = "Большой разброс между костями. Разница составляет" + summ;
+    printLine("result", string);
+
+  }
+};
+
 var total = 0;
 for (var i = 0; i < 15; i++) {
   var first = getRndNumber();
   var second = getRndNumber();
   if (i==8 || i==13)  continue ;
-  document.getElementById("result").innerHTML += "Первая кость:" + first + " Вторая кость:" + second + "</br>";
-  if (first == second) {
-    document.getElementById("result").innerHTML += "Выпал дубль. Число " + first + "<br>";
-  }
-   if (first < 3 && second > 4 ) {
-     var summ = second - first;
-     document.getElementById("result").innerHTML += "Большой разброс между костями. Разница составляет" + summ + "</br>";
-
-   }
-
+  var string = "Первая кость:" + first + " Вторая кость:" + second
+  printLine("result", string);
+  isNumbersEqual(first, second);
+   
    total += first + second;
 
 }
 
-total > 100 ? document.getElementById("result").innerHTML += "Победа, вы набрали "+total+" очков </br>": document.getElementById("result").innerHTML += "Вы проиграли, у вас "+total+ " очков </br>";
+total > 100 ? string= "Победа, вы набрали "+total+" очков": string= "Вы проиграли, у вас "+total+ " очков";
+printLine("result", string);
