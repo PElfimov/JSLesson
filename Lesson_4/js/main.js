07066bac300ba4a43c90206697abf3655467709f
Павел <pelfimov@ya.ru>
2018-05-31 08:25:59 +0700

    что то сделал

--- Lesson_4/js/main.js
+++ Lesson_4/js/main.js
@@ -1,7 +1,11 @@
+function getRndNumber(){
+  return Math.floor((Math.random() * 6) + 1);;
+};
+
 var total = 0;
 for (var i = 0; i < 15; i++) {
-  var first = Math.floor((Math.random() * 6) + 1);
-  var second = Math.floor((Math.random() * 6) + 1);
+  var first = getRndNumber();
+  var second = getRndNumber();
   if (i==8 || i==13)  continue ;
   document.getElementById("result").innerHTML += "Первая кость:" + first + " Вторая кость:" + second + "</br>";
   if (first == second) {
